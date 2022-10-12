48f5b6fa799be95b828a62ba5e8febbee599d78c
diego-tavares <cdiegostavares@gmail.com>
2022-10-11 22:21:19 -0300

    finalizando aula 3

--- js/main.js
+++ js/main.js
@@ -4,21 +4,16 @@ const somar = document.getElementById('somar');
 
 const controle = document.querySelectorAll('.controle-ajuste');
 
-console.log(controle);
-
-somar.addEventListener('click', ()=> {
-  manipulaDados();
+controle.forEach( (elemento)=>{
+   elemento.addEventListener('click', (evento) =>{
+      manipulaDados(evento.target.textContent)
+   })
 })
 
-
- subtrair.addEventListener('click', ()=> {
-   manipulaDados();
- })
-
  function manipulaDados (operacao) {
-   if (operacao === "subtrair") {
-      braco.value = parseInt(braco.value) +1;
-   } else {
+   if (operacao === '-') {
       braco.value = parseInt(braco.value) -1;
+   } else  {
+      braco.value = parseInt(braco.value) +1;
    }
  }
